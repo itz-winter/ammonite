b22bf5e266be5a8779351150e6f43c632cb80dde
kpau <prgrm.yx@gmail.com>
2026-06-01 18:59:57 -0700

    - Added command `/support` - returns support server invite.

---
+++ src/main/java/com/serverbot/commands/utility/SupportCommand.java
@@ -0,0 +1,50 @@
+package com.serverbot.commands.utility;
+
+import com.serverbot.ServerBot;
+import com.serverbot.commands.CommandCategory;
+import com.serverbot.commands.SlashCommand;
+import com.serverbot.utils.EmbedUtils;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+// grab support link from config.json
+
+/**
+ * Support command for providing support link to users
+ */
+
+public class SupportCommand implements SlashCommand {
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        String supportLink = ServerBot.getConfigManager().getConfig().getSupportServerInvite();
+
+        if (supportLink == null || supportLink.isBlank()) {
+            event.replyEmbeds(EmbedUtils.createErrorEmbed(
+                    "Support Link Not Set", "The support link is invalid or has been removed.")).setEphemeral(true).queue();
+            return;
+        }
+
+        event.replyEmbeds(EmbedUtils.createSuccessEmbed(
+                "Support Link", "You can get support for the bot by visiting the following link:\n" + supportLink)).setEphemeral(true).queue();
+    }
+
+    @Override
+    public String getName() {
+        return "support";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Get the support link for the bot";
+    }
+
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.UTILITY;
+    }
+
+    @Override
+    public boolean isGuildOnly() {
+        return false;
+    }
+}
